1a90a7efa06dad3de28dba6a6380624d9df06d15
S-Thiru-Vikram <thiruvikramsde@gmail.com>
2026-02-16 23:11:49 +0530

    update

--- Backend/src/main/java/in/thiruvikram/applytogether/security/JwtUtil.java
+++ Backend/src/main/java/in/thiruvikram/applytogether/security/JwtUtil.java
@@ -72,7 +72,7 @@ public class JwtUtil {
     }
 
     private SecretKey getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        byte[] keyBytes = secret.getBytes(java.nio.charset.StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
